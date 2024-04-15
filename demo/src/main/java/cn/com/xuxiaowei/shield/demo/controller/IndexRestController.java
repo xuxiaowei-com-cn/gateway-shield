ac488936ce975ea2f01fe05a729fa482c69e0e95
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-04-15 16:57:40 +0800

    :white_check_mark: Test Headers

---
+++ demo/src/main/java/cn/com/xuxiaowei/shield/demo/controller/IndexRestController.java
@@ -0,0 +1,39 @@
+package cn.com.xuxiaowei.shield.demo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@RestController
+public class IndexRestController {
+
+	/**
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping
+	public Map<String, Object> index(HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> map = new HashMap<>();
+
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String name = headerNames.nextElement();
+			Enumeration<String> values = request.getHeaders(name);
+			map.put(name, Collections.list(values));
+		}
+
+		return map;
+	}
+
+}
---
+++ gateway/src/test/java/cn/com/xuxiaowei/shield/gateway/demo/IndexRestControllerTests.java
@@ -0,0 +1,54 @@
+package cn.com.xuxiaowei.shield.gateway.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author xuxiaowei
+ * @see org.springframework.http.HttpHeaders
+ * @since 0.0.1
+ */
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexRestControllerTests {
+
+	@LocalServerPort
+	private int serverPort;
+
+	@SneakyThrows
+	@Test
+	void index() {
+
+		String proto = "http";
+		String host = "demo.localdev.me:" + serverPort;
+		String url = String.format("%s://%s", proto, host);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		Map map = restTemplate.getForObject(url, Map.class);
+
+		assertNotNull(map);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+		String value = objectWriter.writeValueAsString(map);
+
+		log.info("Headers: \n{}", value);
+
+		assertEquals(List.of(proto), map.get("x-forwarded-proto"));
+		assertEquals(List.of(host), map.get("x-forwarded-host"));
+	}
+
+}
