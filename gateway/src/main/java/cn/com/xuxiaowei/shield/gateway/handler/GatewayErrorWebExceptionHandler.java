e740ccbd502dc499023acd711b3dfc35d5a5d7d5
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-06-27 20:07:06 +0800

    :sparkles: 网关 异常 处理程序：使用参数启用

--- gateway/src/main/java/cn/com/xuxiaowei/shield/gateway/handler/GatewayErrorWebExceptionHandler.java
+++ gateway/src/main/java/cn/com/xuxiaowei/shield/gateway/handler/GatewayErrorWebExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
@@ -38,6 +39,7 @@ import java.util.Map;
  */
 @Slf4j
 @Component
+@Profile("gateway-error-web-exception-handler")
 public class GatewayErrorWebExceptionHandler implements ErrorWebExceptionHandler, Ordered {
 
 	public static final int ORDERED = Ordered.HIGHEST_PRECEDENCE + 10000;
