c191144ff7e04eeb10a5ad164d8ca5d8fd67783d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-07-01 17:04:11 +0800

    :sparkles: gzip 压缩工具

---
+++ gateway/src/main/java/cn/com/xuxiaowei/shield/gateway/utils/GzipUtils.java
@@ -0,0 +1,50 @@
+package cn.com.xuxiaowei.shield.gateway.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * gzip 压缩工具
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class GzipUtils {
+
+	/**
+	 * 将字符串进行 gzip 压缩
+	 * @param str 要压缩的字符串
+	 * @return 压缩后的字节数组
+	 * @throws IOException 如果压缩过程中发生错误
+	 */
+	public static byte[] compress(String str) throws IOException {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try (GZIPOutputStream gzipOutputStream = new GZIPOutputStream(bos)) {
+			gzipOutputStream.write(str.getBytes());
+		}
+		return bos.toByteArray();
+	}
+
+	/**
+	 * 解压缩 gzip 压缩的字节数组
+	 * @param compressedBytes 压缩后的字节数组
+	 * @return 解压缩后的字节数组
+	 * @throws IOException 如果解压缩过程中发生错误
+	 */
+	public static byte[] decompress(byte[] compressedBytes) throws IOException {
+		ByteArrayInputStream bis = new ByteArrayInputStream(compressedBytes);
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try (GZIPInputStream gzipInputStream = new GZIPInputStream(bis)) {
+			byte[] buffer = new byte[1024];
+			int len;
+			while ((len = gzipInputStream.read(buffer)) != -1) {
+				bos.write(buffer, 0, len);
+			}
+		}
+		return bos.toByteArray();
+	}
+
+}
